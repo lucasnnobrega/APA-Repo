0b36c870c347e0991cb07bf0cfe7fc6f32ccdede
lucasnnobrega <lucasnnobrega@users.noreply.github.com>
2018-09-03 22:02:28 -0300

    remove bad comments

--- Atvd 4/main.py
+++ Atvd 4/main.py
@@ -1,8 +1,6 @@
 # Python program for implementation of heap Sort
 # Um programa em Python para a implementação do heap sort 
 
-# To heapify subtree rooted at index i.
-# n is size of heap
 def heapify(arr, n, i):
     '''
     Arguments:
